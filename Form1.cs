f2d0fe60ac46f64674d51796b94e610be8a7012f
Ali Albayrak <33533989+Hodkan@users.noreply.github.com>
2021-03-02 23:40:18 +0800

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityLayer;
+using DataAccessLayer;
+using LogicLayer;
+
+namespace MultitierArchitecture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnList_Click(object sender, EventArgs e)
+        {
+            List<EntityStaff> StaffList = LogicStaff.LLStaffList();
+            dataGridView1.DataSource = (StaffList);
+
+
+        }
+    }
+}
